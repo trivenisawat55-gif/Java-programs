34f9d8e58b649a7ad8aaf6f2f513a6d436b25448
trivenisawat55-gif <trivenisawat55@gmail.com>
2026-02-16 20:23:29 +0530

    Create Input.java
    To accept input from user in different data types

---
+++ Input.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter your name:");
+        String name=sc.nextLine();
+        System.out.print("enter your age");
+        int age=sc.nextInt();
+      System.out.print("what's your percentage in 12th");
+      double percentage=sc.nextDouble();
+      System.out.print("Are you a student?(true/false):");
+      boolean isStudent=sc.nextboolean();
+System.out.println("Namaskar"+name);
+ System.out.println("you are"+age+"years old"); 
+ System.out.println("your percentage is:"+percentage);  
+ if(isStudent){
+   System.out.println("you are enrolled as a student");
+ }
+else{
+  System.out.println("you are not a student");
+}
+        sc.close();
+      
+    }
+}
