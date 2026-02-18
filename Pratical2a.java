271e5c72c2884d87c150f226caf3be75427eb44c
TRIVENI SAWAT <trivenisawat55@gmail.com>
2026-02-18 15:38:39 +0530

    Create Pratical2a.java

---
+++ Pratical2a.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SquareNumber{
+public static void main(String [ ]args){
+  Scanner scanner=new Scanner(System.in);
+  System.out.println("Enter any number:");
+int num1=scanner.nextInt( );
+int square= num1*num1;
+System.out.println("The square of "+num1+" is "+square);
+ }
+}
